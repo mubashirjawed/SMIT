e61676faaa88488148d5957c49dc669e0e7c6721
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-07 14:08:53 -0700

    Contact Us

---
+++ routes/src/app/contactus/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="text-7xl font-bold text-purple-600 mb-10">Contact Us</h1>
+    </div>
+  );
+};
+
+export default page;
