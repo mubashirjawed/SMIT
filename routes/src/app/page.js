4d3b11dc71a2049511a30362af51cda3f1119379
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-07 14:08:22 -0700

    main

--- routes/src/app/page.js
+++ routes/src/app/page.js
@@ -1,10 +1,33 @@
-import React from 'react';
+"use client";
+
+import Link from "next/link";
+import React from "react";
 
 export default function Home() {
-return(
-  <>
-  <h1>hello</h1>
-  {/* routes in nextjs ........ */}
-  </>
-)
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="text-7xl font-bold text-purple-600 mb-10">Hello</h1>
+
+      <div className="flex gap-8">
+        <Link href="/blogs">
+          <button className="bg-purple-400 text-white font-semibold px-8 py-2 rounded-xl ">
+            blogs
+          </button>
+        </Link>
+
+        <Link href="/aboutus">
+          <button className="bg-purple-400 text-white font-semibold px-8 py-2 rounded-xl ">
+            aboutus
+          </button>
+        </Link>
+
+        <Link href="/contactus">
+          <button className="bg-purple-400 text-white font-semibold px-8 py-2 rounded-xl ">
+            contactus
+          </button>
+        </Link>
+        
+      </div>
+    </div>
+  );
 }
