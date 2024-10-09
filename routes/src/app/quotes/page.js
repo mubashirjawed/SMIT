47c187258a316d3c270ed47f3a0d18c1e7cf5c30
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-09 14:46:49 -0700

    quotes show

--- routes/src/app/quotes/page.js
+++ routes/src/app/quotes/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import QuoteCard from "../components/QuoteCard";
 
 export const metadata = {
   title: "Quotes",
@@ -7,10 +8,13 @@ export const metadata = {
 const Quotes = async () => {
   let quotes = await fetch("https://dummyjson.com/quotes");
   quotes = await quotes.json();
-  
+
   return (
     <div className="p-10">
       <h1 className="font-bold text-3xl text-center">Quotes</h1>
+      {quotes.quotes.map((data) => (
+        <QuoteCard key={data.id} author={data.author} quote={data.quote} />
+      ))}
     </div>
   );
 };
