06808dc6ccfffafc147dd1a19f2eb0fe3c48460c
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-09 14:27:11 -0700

    quotes fetching

--- routes/src/app/quotes/page.js
+++ routes/src/app/quotes/page.js
@@ -5,6 +5,9 @@ export const metadata = {
 };
 
 const Quotes = async () => {
+  let quotes = await fetch("https://dummyjson.com/quotes");
+  quotes = await quotes.json();
+  
   return (
     <div className="p-10">
       <h1 className="font-bold text-3xl text-center">Quotes</h1>
