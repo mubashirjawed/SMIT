ec6c6b7f376f48048d19ceee967dcc3f5bf7a272
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-09 15:07:23 -0700

    some changes

--- routes/src/app/components/QuoteCard.jsx
+++ routes/src/app/components/QuoteCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const QuoteCard = ({ author, quote }) => {
   return (
-    <div className="border m-5 border-pink-200 p-8">
+    <div className="border m-5 cursor-pointer border-pink-200 p-8">
       <h1 className="font-semibold text-2xl">{quote}</h1>
       <h1 className="font-normal text-xl">
         Quote by <span className="font-bold underline">{author}</span>
