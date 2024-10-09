7445a19e8a7774738f9bece806cceca465021566
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-09 15:04:43 -0700

    SingleQuote page done

--- routes/src/app/quotes/[id]/page.js
+++ routes/src/app/quotes/[id]/page.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const SingleQuote = async ({ params }) => {
-  let SingleQuote = await fetch(`https://dummyjson.com/quotes/${params.id}`);
-  SingleQuote = await SingleQuote.json();
+  let singleQuote = await fetch(`https://dummyjson.com/quotes/${params.id}`);
+  singleQuote = await singleQuote.json();
 
-  return <div className="p-10">SingleQuote</div>;
+  return (
+    <div className="p-10">
+      <h1 className="text-center text-5xl font-mono my-5">
+        {singleQuote.quote}
+      </h1>
+      <h1 className="font-thin text-center text-2xl">
+        Quote By
+        <span className="underline font-bold"> {singleQuote.author}</span>
+      </h1>
+    </div>
+  );
 };
 
 export default SingleQuote;
