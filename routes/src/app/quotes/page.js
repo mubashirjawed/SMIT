df22181296a90b9986d32b08ae4f3711353101d6
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-09 14:19:42 -0700

    Quotes-page

---
+++ routes/src/app/quotes/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Quotes = () => {
+  return (
+    <div className="p-10">
+      <h1 className="font-bold text-3xl text-center">Quotes</h1>
+    </div>
+  );
+};
+
+export default Quotes;
