99f2e794c5ce98a2932513608c46a9ebd2396fd8
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-14 14:25:19 -0700

    input UI

--- todo-webapp/src/app/todos/page.js
+++ todo-webapp/src/app/todos/page.js
@@ -9,6 +9,15 @@ const Todos = async () => {
   return (
     <div className="min-h-screen p-10">
       <h1 className="text-3xl text-center font-bold">Todos</h1>
+      <div className=" w-2/3 mx-auto flex gap-2">
+        <input
+          placeholder="Enter Todo"
+          type="text"
+          className="border-2 p-2 flex flex-grow"
+        />
+        <button className="bg-purple-200 rounded p-2 px-4">Add Todo</button>
+      </div>
+
       {res.data?.map((todo) => (
         <Link key={todo.id} href={`/todos/${todo.id}`}>
           <h1 className="border p-2 text-center text-3xl w-2/3 mx-auto my-1">
