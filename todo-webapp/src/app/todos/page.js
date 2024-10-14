4e70d74a60e59019c41957c66915d1911f657df6
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-14 13:57:06 -0700

    cache added

--- todo-webapp/src/app/todos/page.js
+++ todo-webapp/src/app/todos/page.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
 const Todos = async () => {
-  let res = await fetch("http://localhost:3000/api/todos");
+  let res = await fetch("http://localhost:3000/api/todos", {
+    cache: "no-cache",
+  });
   res = await res.json();
 
   return (
     <div className="min-h-screen p-10">
       <h1 className="text-3xl text-center font-bold">Todos</h1>
       {res.data?.map((todo) => (
-        <Link href={`/todos/${todo}`}>
+        <Link key={todo.id} href={`/todos/${todo.id}`}>
           <h1 className="border p-2 text-center text-3xl w-2/3 mx-auto my-1">
-            {todo}
+            {todo.todo}
           </h1>
         </Link>
       ))}
