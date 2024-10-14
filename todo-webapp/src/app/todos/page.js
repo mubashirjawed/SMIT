345d8ac022f8d8d4da386f0c736e1042fb719075
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-14 13:42:00 -0700

    render data on Home page

--- todo-webapp/src/app/todos/page.js
+++ todo-webapp/src/app/todos/page.js
@@ -1,5 +1,21 @@
-const Todos = () => {
-  return <div>Todos</div>;
+import Link from "next/link";
+
+const Todos = async () => {
+  let res = await fetch("http://localhost:3000/api/todos");
+  res = await res.json();
+
+  return (
+    <div className="min-h-screen p-10">
+      <h1 className="text-3xl text-center font-bold">Todos</h1>
+      {res.data?.map((todo) => (
+        <Link href={`/todos/${todo}`}>
+          <h1 className="border p-2 text-center text-3xl w-2/3 mx-auto my-1">
+            {todo}
+          </h1>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Todos;
