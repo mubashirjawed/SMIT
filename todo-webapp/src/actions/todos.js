9b113063f5e831dfbca5b880f1025dbba2e32fdc
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-15 12:29:51 -0700

    server

--- todo-webapp/src/actions/todos.js
+++ todo-webapp/src/actions/todos.js
@@ -1,6 +1,17 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function addTodo(formData) {
   const todo = formData.get("todo");
-  console.log("todo =>", todo);
+  try {
+    await fetch("http://localhost:3000/api/todos", {
+      method: "POST",
+      body: JSON.stringify({ todo }),
+    });
+
+    revalidatePath("/todos");
+  } catch (error) {
+    console.log(error);
+  }
 }
