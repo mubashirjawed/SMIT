859a94e1ad356c633e9f6182964bc2db2d731f70
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-15 14:28:11 -0700

    added buttons

--- todo-webapp/src/components/ListItem.js
+++ todo-webapp/src/components/ListItem.js
@@ -1,8 +1,23 @@
 const ListItem = ({ todo }) => {
+    
   return (
-    <h1 className="border p-2 text-center text-3xl w-2/3 mx-auto my-1">
-      {todo.todo}
-    </h1>
+    <div
+      className={`border flex cursor-pointer p-2 text-center text-3xl w-2/3 mx-auto my-1 ${
+        todo.isCompleted ? "bg-teal-100" : "bg-white"
+      }`}
+    >
+      <h1 className="flex flex-grow">{todo.todo}</h1>
+
+      <button className="bg-fuchsia-200 mx-2  p-1 px-2 text-sm rounded">
+        {!todo.isCompleted ? "Done" : "Not Done"}
+      </button>
+      <button className="bg-blue-200 mx-2  p-1 px-2 text-sm rounded">
+        Edit
+      </button>
+      <button className="bg-red-200  mx-2 p-1 px-2 text-sm rounded">
+        Delete
+      </button>
+    </div>
   );
 };
 
