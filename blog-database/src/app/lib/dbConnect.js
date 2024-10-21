ab344b031ce2c5e165fa1d87abad2495c169d3e1
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-21 13:28:00 +0500

    ConnectDB

--- blog-database/src/app/lib/dbConnect.js
+++ blog-database/src/app/lib/dbConnect.js
@@ -3,10 +3,12 @@ import mongoose, { connect } from "mongoose";
 export async function ConnectDB() {
   let isConnected = false;
   if (isConnected) {
-    
+    return "DB is alrealy connected"
   }
   try {
     let Connected = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB is Connected Successfully..");
+    
     if(Connected.connection.readyState == 1) isConnected = true;
   } catch (error) {
     console.log(error);
