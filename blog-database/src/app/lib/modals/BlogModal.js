4de0eb1df8550fb3e37159dd3997c8d101c3392d
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-22 12:56:04 +0500

    timestamps

--- blog-database/src/app/lib/modals/BlogModal.js
+++ blog-database/src/app/lib/modals/BlogModal.js
@@ -1,8 +1,12 @@
 const { default: mongoose } = require("mongoose");
 
-const BlogSchema = new mongoose.Schema({
-  title: { type: String, unique: true },
-  body: String,
-  createdAt: { type: Date, default: Date.now },
-  author: String,
-});
+const BlogSchema = new mongoose.Schema(
+  {
+    title: { type: String, unique: true },
+    body: String,
+    author: String,
+  },
+  {
+    timestamps: true,
+  }
+);
