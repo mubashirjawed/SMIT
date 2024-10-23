2b59181a6e5c4dadecb4de4293cd9b38bee7c091
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-10-23 14:04:52 +0500

    some error

--- blog-database/src/app/api/blogs/route.js
+++ blog-database/src/app/api/blogs/route.js
@@ -1,9 +1,13 @@
 import { ConnectDB } from "@/app/lib/dbConnect";
+import BlogModal from "@/app/lib/modals/BlogModal";
 
 export async function GET(request) {
   await ConnectDB();
+  const blogs = await BlogModal.find();
+  console.log("MongoDB =>", blogs);
+
   return Response.json({
-    data: [],
+    data: blogs,
     msg: "Blogs fetched Succssfully",
   });
 }
