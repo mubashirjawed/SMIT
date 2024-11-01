4b9a26498dee53d90e13cc4349f24b07df4bbf4b
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-01 16:36:58 +0500

    fetching

--- techzeen/src/app/loading-data/page.js
+++ techzeen/src/app/loading-data/page.js
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
+
+async function myList() {
+  let Data = fetch("https://jsonplaceholder.typicode.com/users");
+  Data = await Data.json();
+  return Data;
+}
 
 const LoadingData = () => {
-  return (
+
+    return (
     <>
       <h2>Loading Data </h2>
     </>
