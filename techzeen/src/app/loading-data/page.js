82b49daf560561a6a18d01267f88a08368946991
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-01 17:22:45 +0500

    done

--- techzeen/src/app/loading-data/page.js
+++ techzeen/src/app/loading-data/page.js
@@ -5,7 +5,7 @@ import Loader from "./loader";
 async function myList() {
   let data = await fetch("https://jsonplaceholder.typicode.com/users");
   data = await data.json();
-  return data; // return the array directly
+  return data;
 }
 
 const LoadingData = () => {
