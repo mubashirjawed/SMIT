398497b0231c34df56a2430e4de471052bf447f8
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-06 14:12:16 +0500

    Register info

--- techzeen/src/app/register/page.js
+++ techzeen/src/app/register/page.js
@@ -1,6 +1,15 @@
-
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="p-10 bg-gray-400 flex flex-col">
+      <h2>Register Users</h2>
+      <br />
+      <input type="text" placeholder=" Enter your name" />
+      <br />
+      <input type="number" placeholder=" Enter your Age" />
+      <br />
+      <button>Register</button>
+    </div>
+  );
 };
 
 export default Register;
