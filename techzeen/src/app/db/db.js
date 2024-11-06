6567ada7611cced36e166380282f7ed5f8d9c408
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-06 12:01:52 +0500

    data

---
+++ techzeen/src/app/db/db.js
@@ -0,0 +1,20 @@
+export const employee = [
+  {
+    employeeId: 1,
+    name: "Ali",
+    salary: "70K",
+    email: "ali@gmail.com",
+  },
+  {
+    employeeId: 2,
+    name: "Moiz",
+    salary: "90K",
+    email: "moiz@gmail.com",
+  },
+  {
+    employeeId: 3,
+    name: "bilal",
+    salary: "50K",
+    email: "bilal@gmail.com",
+  },
+];
