22bf8f59620287998506983257755c5b8cc46177
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-06 14:25:20 +0500

    info done

--- techzeen/src/app/register/page.js
+++ techzeen/src/app/register/page.js
@@ -1,13 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const registerUser = () => {
+    console.log(name, age);
+  };
   return (
     <div className="p-10 bg-gray-400 flex flex-col">
       <h2>Register Users</h2>
       <br />
-      <input type="text" placeholder=" Enter your name" />
+      <input
+        type="text"
+        placeholder=" Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <br />
-      <input type="number" placeholder=" Enter your Age" />
+      <input
+        type="number"
+        placeholder=" Enter your Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
       <br />
-      <button>Register</button>
+      <button onClick={registerUser}>Register</button>
     </div>
   );
 };
