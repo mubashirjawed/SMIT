b242247a2244866997afd5fdc54466db1371b6df
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-06 14:52:37 +0500

    pending

---
+++ techzeen/src/app/api/users/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (req, res) => {
+  let data = await req.json();
+  console.log(data);
+  return NextResponse.json({ result: "Sucessfull" }, { status: 200 });
+};
