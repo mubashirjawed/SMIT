1c1257e0fab06696f0d315eaf6998330a591ad77
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-06 11:44:47 +0500

    some changes

--- techzeen/src/app/static-assets/page.js
+++ techzeen/src/app/static-assets/page.js
@@ -4,7 +4,13 @@ const StaticAssets = () => {
   return (
     <>
       <div className="text-5xl bg-slate-300">StaticAssets</div>
-      <Image className="p-6" src={"./next.svg"} alt="assets" width={200} height={200} />
+      <Image
+        className="p-6"
+        src={"./next.svg"}
+        alt="assets"
+        width={200}
+        height={200}
+      />
     </>
   );
 };
