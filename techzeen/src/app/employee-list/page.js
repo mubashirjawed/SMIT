c230c6a5321da181318ed444d040d35f0d580711
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-15 15:34:27 +0500

    id

--- techzeen/src/app/employee-list/page.js
+++ techzeen/src/app/employee-list/page.js
@@ -1,3 +1,5 @@
+import DeleteButton from "../components/DeleteButton";
+
 const getEmployees = async () => {
   let data = await fetch("http://localhost:3000/api/employee");
   data = await data.json();
@@ -13,6 +15,7 @@ const EmployeeList = async () => {
       {employees.map((item, i) => {
         <div key={i}>
           <h3>{item.name}</h3>
+          <DeleteButton id={item.employeeId}/>
         </div>;
       })}
     </>
