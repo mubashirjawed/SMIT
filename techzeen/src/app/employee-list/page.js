ee41312526025056957367aac3933d6823a065f8
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-15 15:28:19 +0500

    employee-list

--- techzeen/src/app/employee-list/page.js
+++ techzeen/src/app/employee-list/page.js
@@ -1,11 +1,22 @@
-import React from "react";
 const getEmployees = async () => {
   let data = await fetch("http://localhost:3000/api/employee");
   data = await data.json();
   return data;
 };
-const EmployeeList = () => {
-  return <div></div>;
+
+const EmployeeList = async () => {
+  const employees = await getEmployees();
+
+  return (
+    <>
+      <h1>employee-list</h1>
+      {employees.map((item, i) => {
+        <div key={i}>
+          <h3>{item.name}</h3>
+        </div>;
+      })}
+    </>
+  );
 };
 
 export default EmployeeList;
