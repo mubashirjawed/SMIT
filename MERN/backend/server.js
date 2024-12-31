fa6aa6dd5e8ee5ad776e3e226e72f4e982c4f9b9
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-12-31 22:48:06 +0500

    checking.....

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("api runing");
+  res.send("api runing in the bankend");
 });
 
 app.listen(4000);
