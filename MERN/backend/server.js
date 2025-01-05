00a9488a47fbcf6c8402c7d0a00059677ff83905
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2025-01-05 12:01:16 +0500

    working....

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -19,6 +19,20 @@ mongoose
 
 app.post("/", async (req, res) => {
   const [name, email, age] = req.body;
+
+  const User = require("./models/userModel");
+
+  try {
+    const userAdded = await User.create({
+      name: name,
+      email: email,
+      age: age,
+    });
+    res.status(201).json(userAdded);
+  } catch (error) {
+    console.log(error);
+    res.send(400).json({ error: error.message });
+  }
 });
 
 // ----------------- -------------
