0e363a3ad57300ddc575cbc853909f2cf6e6dd5d
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2025-01-05 11:48:17 +0500

    working.......

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const User = require("./models/userModel")
+const User = require("./models/userModel");
 
 mongoose
   .connect(process.env.URI)
@@ -14,11 +14,17 @@ mongoose
   .catch((err) => {
     console.log("error", err);
   });
+
+// create
+
+app.post("/", async (req, res) => {
+  const [name, email, age] = req.body;
+});
+
+// ----------------- -------------
 app.get("/", (req, res) => {
   res.send("api runing in the bankend");
 });
-
-
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.log("error", err);
