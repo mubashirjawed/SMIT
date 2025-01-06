4ac5e87d8037886c285962a0b6322c3614ed9666
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2025-01-06 13:40:43 +0500

    working.....

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const User = require("./models/userModel");
+const userRoute = require("./routes/userRoute");
 
 dotenv.config();
-
-const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // Connect to MongoDB
@@ -13,42 +12,14 @@ mongoose
   .connect(process.env.URI)
   .then(() => {
     console.log("Connected to MongoDB");
+    // Start the Server
+    const PORT = process.env.PORT || 6000;
+    app.listen(PORT, () => {
+      console.log(`Server running successfully on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
 
-// Create User POST
-app.post("/", async (req, res) => {
-  // Destructure from req.body
-  const { name, email, age } = req.body;
-
-  try {
-    const userAdded = await User.create({
-      name: name,
-      email: email,
-      age: age,
-    });
-    res.status(201).json(userAdded);
-  } catch (error) {
-    console.log(error);
-    res.send(400).json({ error: error.message });
-  }
-});
-
-// Default Route GET
-app.get("/", async (req, res) => {
-  try {
-    const ShowAll = await User.find();
-    res.status(200).json(ShowAll);
-  } catch (error) {
-    console.log(error);
-    res.send(500).json({ error: error.message });
-  }
-});
-
-// Start the Server
-const PORT = process.env.PORT || 6000;
-app.listen(PORT, () => {
-  console.log(`Server running successfully on port ${PORT}`);
-});
+app.use(userRoute);
