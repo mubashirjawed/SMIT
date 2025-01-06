2ac76cf0c4974f5c9774f7e9b2a937bd22492632
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2025-01-06 13:00:48 +0500

    modal

--- MERN/backend/models/userModel.js
+++ MERN/backend/models/userModel.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
 // Create Schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    age: {
+      type: Number,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    require: true,
-  },
-  age: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 // Create Modal
 const User = mongoose.model("User", userSchema);
